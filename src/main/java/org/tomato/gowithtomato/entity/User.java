6dc592f5784a2fc3ffa87aee4580bf9be9e3b96b
khairullovruslan <khairullov.ruslan@gmail.com>
2024-10-09 11:19:14 +0300

    feat: added lombok annotations for entity

--- src/main/java/org/tomato/gowithtomato/entity/User.java
+++ src/main/java/org/tomato/gowithtomato/entity/User.java
@@ -1,5 +1,14 @@
 package org.tomato.gowithtomato.entity;
 
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
     private long id;
     private String login;
