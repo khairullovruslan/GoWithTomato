8a7508c413e1612e357c1284f4e18ff569c7370b
khairullovruslan <khairullov.ruslan@gmail.com>
2024-10-09 16:32:53 +0300

    feat: added session and cookie after success auth

--- src/main/java/org/tomato/gowithtomato/service/AuthService.java
+++ src/main/java/org/tomato/gowithtomato/service/AuthService.java
@@ -1,5 +1,7 @@
 package org.tomato.gowithtomato.service;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.tomato.gowithtomato.dao.UserDAOImpl;
@@ -12,17 +14,22 @@ public class AuthService {
     private final UserDAOImpl userDao;
     private final  ModelMapper mapper;
     private final PasswordUtil passwordUtil;
+    private final SessionAndCookieService sessionAndCookieService;
 
 
     public AuthService() {
+        this.sessionAndCookieService = SessionAndCookieService.getInstance();
         this.passwordUtil = PasswordUtil.getInstance();
         this.userDao = UserDAOImpl.getInstance();
         this.mapper = new ModelMapper();
     }
-    public void registration(UserDTO userDTO){
+    public void registration(UserDTO userDTO, HttpServletRequest request, HttpServletResponse response){
         String password = passwordUtil.hashPassword(userDTO.getPassword());
         userDTO.setPassword(password);
         userDao.save(mapper.map(userDTO, User.class));
+        sessionAndCookieService.createSession(request, userDTO.getLogin());
+        sessionAndCookieService.setCookie(response, userDTO.getLogin());
         log.info("Пользователь успешно зарегестрирован");
+
     }
 }
