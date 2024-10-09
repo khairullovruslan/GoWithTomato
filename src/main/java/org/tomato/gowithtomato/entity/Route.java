bba3925f8cde69c8426fae3722e0402e516247d6
khairullovruslan <khairullov.ruslan@gmail.com>
2024-10-09 11:30:03 +0300

    feat: added entity for route table

---
+++ src/main/java/org/tomato/gowithtomato/entity/Route.java
@@ -0,0 +1,17 @@
+package org.tomato.gowithtomato.entity;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Route {
+    private Long id;
+    private String departurePoint;
+    private String destinationPoint;
+    private String intermediateStops;
+    private double distance;
+}
