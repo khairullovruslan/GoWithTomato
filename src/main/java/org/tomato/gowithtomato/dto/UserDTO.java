4670b2f7affd6da47fd531bd4aac50f9c5caf691
khairullovruslan <khairullov.ruslan@gmail.com>
2024-10-09 15:29:32 +0300

    feat: added annotation for login and email validation

--- src/main/java/org/tomato/gowithtomato/dto/UserDTO.java
+++ src/main/java/org/tomato/gowithtomato/dto/UserDTO.java
@@ -1,16 +1,22 @@
 package org.tomato.gowithtomato.dto;
 
+import jakarta.validation.constraints.Email;
 import lombok.*;
+import org.tomato.gowithtomato.validator.annotations.ValidLogin;
 import org.tomato.gowithtomato.validator.annotations.ValidPassword;
 
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDTO{
-    private String login;
 
+    @ValidLogin
+    private String login;
     @ValidPassword
     private String password;
+    @Email
     private String email;
     private String phoneNumber;
 }
