41ed778f507881dc9e2a97a9bf0f0a4cfce9c17c
khairullovruslan <khairullov.ruslan@gmail.com>
2024-10-09 18:47:20 +0300

    feat: added servlet for logout

---
+++ src/main/java/org/tomato/gowithtomato/controller/auth/LogoutServlet.java
@@ -0,0 +1,24 @@
+package org.tomato.gowithtomato.controller.auth;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.tomato.gowithtomato.controller.BaseServlet;
+import org.tomato.gowithtomato.service.AuthService;
+
+import java.io.IOException;
+
+
+@WebServlet("/logout")
+public class LogoutServlet extends BaseServlet {
+    private AuthService authService;
+    @Override
+    public void init() {
+        authService = (AuthService) this.getServletContext().getAttribute("authService");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        authService.logout(req, resp);
+    }
+}
