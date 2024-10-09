01ec874b876bdabe024fc0fc6bff0f16b9fa4973
khairullovruslan <khairullov.ruslan@gmail.com>
2024-10-09 16:33:26 +0300

    refactor: change redirect url

--- src/main/java/org/tomato/gowithtomato/controller/auth/SignUpServlet.java
+++ src/main/java/org/tomato/gowithtomato/controller/auth/SignUpServlet.java
@@ -36,7 +36,7 @@ public class SignUpServlet extends BaseServlet {
         if (!violations.isEmpty()) {
             throw new RegistrationException(violations);
         }
-        authService.registration(userDTO);
-        resp.sendRedirect("/home");
+        authService.registration(userDTO, req, resp);
+        resp.sendRedirect(req.getContextPath() + "/home");
     }
 }
