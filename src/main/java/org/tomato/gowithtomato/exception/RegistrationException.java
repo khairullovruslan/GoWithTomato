1dfd92f7952845781af55b4a01aa43616d3532ba
khairullovruslan <khairullov.ruslan@gmail.com>
2024-10-09 15:04:20 +0300

    feat: added  custom exception for registration fail

---
+++ src/main/java/org/tomato/gowithtomato/exception/RegistrationException.java
@@ -0,0 +1,16 @@
+package org.tomato.gowithtomato.exception;
+
+import jakarta.validation.ConstraintViolation;
+import lombok.Getter;
+import org.tomato.gowithtomato.dto.UserDTO;
+
+import java.util.Set;
+
+@Getter
+public class RegistrationException extends RuntimeException {
+    private final Set<ConstraintViolation<UserDTO>> violations;
+
+    public RegistrationException(Set<ConstraintViolation<UserDTO>> violations) {
+        this.violations = violations;
+    }
+}
