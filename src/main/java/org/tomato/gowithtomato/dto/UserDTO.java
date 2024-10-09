e07494ec1ba0380da022c729ee7c7afb05a75d48
khairullovruslan <khairullov.ruslan@gmail.com>
2024-10-09 15:05:47 +0300

    feat: added annotation for pwd validation

--- src/main/java/org/tomato/gowithtomato/dto/UserDTO.java
+++ src/main/java/org/tomato/gowithtomato/dto/UserDTO.java
@@ -1,12 +1,15 @@
 package org.tomato.gowithtomato.dto;
 
 import lombok.*;
+import org.tomato.gowithtomato.validator.annotations.ValidPassword;
 
 @Getter
 @Setter
 @Builder
 public class UserDTO{
     private String login;
+
+    @ValidPassword
     private String password;
     private String email;
     private String phoneNumber;
