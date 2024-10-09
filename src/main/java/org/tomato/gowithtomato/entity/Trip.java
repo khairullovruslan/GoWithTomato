eef85c52abc9e131f16236a42ef6dbf32d0beb1f
khairullovruslan <khairullov.ruslan@gmail.com>
2024-10-09 11:30:11 +0300

    feat: added entity for trip table

---
+++ src/main/java/org/tomato/gowithtomato/entity/Trip.java
@@ -0,0 +1,24 @@
+package org.tomato.gowithtomato.entity;
+
+
+import lombok.*;
+import org.tomato.gowithtomato.dto.TripStatus;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Trip {
+    private Long id;
+    private User owner;
+    private Route route;
+    private LocalDateTime tripDateTime;
+    private int availableSeats;
+    private BigDecimal price;
+    private TripStatus status;
+}
