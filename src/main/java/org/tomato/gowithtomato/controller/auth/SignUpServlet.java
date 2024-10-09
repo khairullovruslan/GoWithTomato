0fbf9e2389d4c4f2a9fbd6d0f8bfe02063f0b7fb
khairullovruslan <khairullov.ruslan@gmail.com>
2024-10-09 15:04:58 +0300

    refactor: delete dto mapper from servlet

--- src/main/java/org/tomato/gowithtomato/controller/auth/SignUpServlet.java
+++ src/main/java/org/tomato/gowithtomato/controller/auth/SignUpServlet.java
@@ -4,9 +4,14 @@ package org.tomato.gowithtomato.controller.auth;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.ConstraintViolation;
 import org.tomato.gowithtomato.controller.BaseServlet;
 import org.tomato.gowithtomato.dto.UserDTO;
+import org.tomato.gowithtomato.exception.RegistrationException;
 import org.tomato.gowithtomato.service.AuthService;
+import org.tomato.gowithtomato.util.UserUtil;
+
+import java.util.Set;
 
 @WebServlet("/sign-up")
 public class SignUpServlet extends BaseServlet {
@@ -25,18 +30,11 @@ public class SignUpServlet extends BaseServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp){
-        String login = req.getParameter("login");
-        String pwd = req.getParameter("pwd");
-        String email = req.getParameter("email");
-        String phone = req.getParameter("phone");
-
-        UserDTO userDTO = UserDTO
-                .builder()
-                .password(pwd)
-                .login(login)
-                .phoneNumber(phone)
-                .email(email)
-                .build();
+        UserDTO userDTO = UserUtil.getInstance().buildUserDTO(req);
+        Set<ConstraintViolation<UserDTO>> violations = validator.validate(userDTO);
+        if (!violations.isEmpty()) {
+            throw new RegistrationException(violations);
+        }
         authService.registration(userDTO);
     }
 }
