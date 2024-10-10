18ac53f09ce8de98637e648ee4a61fe6d9043d78
khairullovruslan <khairullov.ruslan@gmail.com>
2024-10-10 17:23:37 +0300

    feat: servlet for new route page

---
+++ src/main/java/org/tomato/gowithtomato/controller/RouteServlet.java
@@ -0,0 +1,27 @@
+package org.tomato.gowithtomato.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.tomato.gowithtomato.dto.RouteDTO;
+import org.tomato.gowithtomato.util.AjaxUtil;
+
+import java.io.IOException;
+
+@WebServlet("/new-route")
+public class RouteServlet extends BaseServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+        processTemplate("route", req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        AjaxUtil ajaxUtil =  AjaxUtil.getInstance();
+        RouteDTO routeDTO = objectMapper.readValue(req.getInputStream(), RouteDTO.class);
+        ajaxUtil.senderRespUrl(req.getContextPath() + "/profile", resp);
+    }
+}
