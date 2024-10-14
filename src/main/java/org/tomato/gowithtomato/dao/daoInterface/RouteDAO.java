afd20b429a7b650abbd3d5ecc8d62486ff59937d
khairullovruslan <khairullov.ruslan@gmail.com>
2024-10-14 17:40:39 +0300

    feat: added find by user func

--- src/main/java/org/tomato/gowithtomato/dao/daoInterface/RouteDAO.java
+++ src/main/java/org/tomato/gowithtomato/dao/daoInterface/RouteDAO.java
@@ -1,6 +1,10 @@
 package org.tomato.gowithtomato.dao.daoInterface;
 
 import org.tomato.gowithtomato.entity.Route;
+import org.tomato.gowithtomato.entity.User;
+
+import java.util.List;
 
 public interface RouteDAO extends CrudDao<Long, Route> {
+    List<Route> findByUser(User id);
 }
