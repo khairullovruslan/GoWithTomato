0e40b24fc251e4a9899592b43b859a3330ac144c
khairullovruslan <khairullov.ruslan@gmail.com>
2024-10-16 10:57:07 +0300

    feat: added search function for all trips and search with filtering

--- src/main/java/org/tomato/gowithtomato/dao/TripDAOImpl.java
+++ src/main/java/org/tomato/gowithtomato/dao/TripDAOImpl.java
@@ -1,26 +1,33 @@
 package org.tomato.gowithtomato.dao;
 
+import lombok.Cleanup;
+import lombok.SneakyThrows;
 import org.tomato.gowithtomato.dao.daoInterface.TripDAO;
-import org.tomato.gowithtomato.entity.Trip;
-import org.tomato.gowithtomato.entity.TripStatus;
+import org.tomato.gowithtomato.entity.*;
 import org.tomato.gowithtomato.exception.DaoException;
 
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.sql.Types;
-import java.util.List;
-import java.util.Optional;
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 public class TripDAOImpl implements TripDAO {
     private final static TripDAOImpl INSTANCE = new TripDAOImpl();
+    private final UserDAOImpl userDAO;
+    private final RouteDAOImpl routeDAO;
 
     private TripDAOImpl(){
-
+        userDAO = UserDAOImpl.getInstance();
+        routeDAO = RouteDAOImpl.getInstance();
     }
     private final static String  SAVE_SQL =
             """
                  INSERT  INTO trip(USER_ID, ROUTE_ID, TRIP_DATE_TIME, AVAILABLE_SEATS, PRICE, STATUS)
                  VALUES (?, ?, ?, ?, ?, ?)
+            """;
+    private final static String FIND_ALL_SQL =
+            """
+            SELECT * from trip
             """;
 
     public static TripDAOImpl getInstance() {
@@ -46,7 +53,6 @@ public class TripDAOImpl implements TripDAO {
             throw new DaoException();
 
         } catch (SQLException e) {
-            e.printStackTrace();
             throw new RuntimeException(e);
         }
     }
@@ -56,10 +62,19 @@ public class TripDAOImpl implements TripDAO {
         return Optional.empty();
     }
 
+    @SneakyThrows
     @Override
     public List<Trip> findAll() {
-        return List.of();
+        @Cleanup Connection connection = connectionManager.get();
+        return findAll(connection);
+
     }
+    public List<Trip> findAll(Connection connection) throws SQLException {
+        var statement = connection.prepareStatement(FIND_ALL_SQL);
+        var result = statement.executeQuery();
+        return convertResultSetToList(connection, result);
+    }
+
 
     @Override
     public Trip save(Trip entity) throws DaoException {
@@ -74,5 +89,66 @@ public class TripDAOImpl implements TripDAO {
     @Override
     public void delete(Long id) {
 
+    }
+
+    private List<Trip> convertResultSetToList(Connection connection, ResultSet result) throws SQLException {
+        List<Trip> points = new ArrayList<>();
+        while (result.next()) {
+            Optional<User> owner = userDAO.findById(connection, result.getLong("user_id"));
+            Optional<Route> route = routeDAO.findById(connection, result.getLong("route_id"));
+            if (owner.isEmpty() || route.isEmpty()){
+                throw  new DaoException();
+            }
+            points.add(Trip.builder()
+                    .id(result.getLong("id"))
+                    .price(result.getBigDecimal("price"))
+                    .availableSeats(result.getInt("available_seats"))
+                    .tripDateTime(result.getTimestamp("trip_date_time").toLocalDateTime())
+                    .status(TripStatus.valueOf(result.getString("status")))
+                    .owner(owner.get())
+                    .route(route.get())
+                    .build());
+
+        }
+        return points;
+    }
+
+    @SneakyThrows
+    public List<Trip> findAllByFilter(Map<String, String> filter) {
+        @Cleanup var connection = connectionManager.get();
+        return findAllByFilter(connection, filter);
+    }
+
+
+    public List<Trip> findAllByFilter(Connection connection, Map<String, String> filter) throws SQLException {
+       String query = getQueryByFilter(filter);
+       var statement = connection.prepareStatement(query);
+       return convertResultSetToList(connection, statement.executeQuery());
+    }
+
+    private String getQueryByFilter(Map<String, String> filter){
+        StringBuilder query = new StringBuilder(
+                """
+                SELECT t.*
+                FROM trip t
+                         JOIN route r ON t.route_id = r.id
+                         JOIN Point p1 ON r.start_point_id = p1.id
+                         JOIN Point p2 ON r.finish_point_id = p2.id
+                """);
+        if (!filter.isEmpty()) query.append(" WHERE ");
+        String[] s = new String[filter.keySet().size()];
+        int count = 0;
+        for (String key : filter.keySet()){
+            switch (key) {
+                case "from" -> s[count++] = String.format("p1.name = '%s'", filter.get(key));
+                case "to" -> s[count++] = String.format("p2.name = '%s'", filter.get(key));
+                case "count" -> s[count++] = String.format("t.available_seats >= '%s'", filter.get(key));
+                case "date" -> {
+                    s[count++] = String.format("t.trip_date_time >= '%s'", LocalDateTime.parse(filter.get(key)).format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+                }
+            }
+        }
+        query.append(String.join(" and ", s));
+        return query.toString();
     }
 }
