07aaa487116ed2a08116cd431dfa562380005fe6
khairullovruslan <khairullov.ruslan@gmail.com>
2024-10-16 10:58:01 +0300

    feat: added service for trip

--- src/main/java/org/tomato/gowithtomato/service/TripService.java
+++ src/main/java/org/tomato/gowithtomato/service/TripService.java
@@ -2,7 +2,11 @@ package org.tomato.gowithtomato.service;
 
 import org.tomato.gowithtomato.dao.TripDAOImpl;
 import org.tomato.gowithtomato.dto.TripDTO;
+import org.tomato.gowithtomato.entity.Trip;
 import org.tomato.gowithtomato.mapper.TripMapper;
+
+import java.util.List;
+import java.util.Map;
 
 public class TripService {
     private final static TripService INSTANCE = new TripService();
@@ -16,7 +20,16 @@ public class TripService {
     public static TripService getInstance() {
         return INSTANCE;
     }
-    public TripDTO saveTrip(TripDTO tripDTO, Long id){
-        return tripMapper.convertTripToDTO(tripDAO.save(tripMapper.convertDTOToTrip(tripDTO), id));
+    public void saveTrip(TripDTO tripDTO, Long id){
+        tripDAO.save(tripMapper.convertDTOToTrip(tripDTO), id);
+    }
+    public List<TripDTO> findAll(){
+        List<Trip> trips = tripDAO.findAll();
+        return trips.stream().map(tripMapper::convertTripToDTO).toList();
+    }
+
+    public List<TripDTO> findByFilter(Map<String, String> filter) {
+        List<Trip> trips = tripDAO.findAllByFilter(filter);
+        return trips.stream().map(tripMapper::convertTripToDTO).toList();
     }
 }
