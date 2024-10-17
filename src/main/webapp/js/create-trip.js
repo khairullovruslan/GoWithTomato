614024f1c648d82f7bf4b51eb90ae1449a1f5a8a
khairullovruslan <khairullov.ruslan@gmail.com>
2024-10-17 16:07:40 +0300

    refactor: created script file for create trip view

---
+++ src/main/webapp/js/create-trip.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+    $('#route-form').on('submit', function (event) {
+        event.preventDefault();
+
+        const tripData = {
+            tripDateTime: $('#tripDateTime').val(),
+            availableSeats: $('#availableSeats').val(),
+            price: parseFloat($('#price').val())
+        };
+        const contextPath = "/gowithtomato";
+        const routeId = $('#routeId').val();
+
+        console.log(routeId);
+
+        const path = contextPath + '/create-trip?id=' + routeId ;
+        console.log(path);
+
+        $.ajax({
+            url: path,
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(tripData),
+            success: function (response) {
+                if (response && response.url) {
+                    window.location.href = response.url;
+                } else {
+                    console.error('URL не найден в ответе:', response);
+                }
+            },
+            error: function (xhr, status, error) {
+                console.error('Ошибка:', status, error);
+            }
+        });
+    });
+});
