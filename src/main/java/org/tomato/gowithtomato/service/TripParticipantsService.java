7fd9f8a59e8df8f0487c5a4c05383d3b2db9b63f
khairullovruslan <khairullov.ruslan@gmail.com>
2024-10-17 16:09:39 +0300

    feat: added  service for m2m table

---
+++ src/main/java/org/tomato/gowithtomato/service/TripParticipantsService.java
@@ -0,0 +1,26 @@
+package org.tomato.gowithtomato.service;
+
+import org.tomato.gowithtomato.dao.TripParticipantsDAOImpl;
+import org.tomato.gowithtomato.dto.UserDTO;
+import org.tomato.gowithtomato.entity.User;
+import org.tomato.gowithtomato.mapper.UserMapper;
+
+import java.util.List;
+
+public class TripParticipantsService {
+    private final static TripParticipantsService INSTANCE = new TripParticipantsService();
+    private final UserMapper userMapper;
+    private final TripParticipantsDAOImpl tripParticipantsDAO;
+    private TripParticipantsService(){
+        userMapper = UserMapper.getInstance();
+        tripParticipantsDAO = TripParticipantsDAOImpl.getInstance();
+    }
+
+    public static TripParticipantsService getInstance() {
+        return INSTANCE;
+    }
+    public List<UserDTO> findUsersByTripId(Long id){
+        List<User> users = tripParticipantsDAO.findUsersByTripId(id);
+        return users.stream().map(userMapper::convertUserToDTO).toList();
+    }
+}
