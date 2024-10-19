3e0a5b471d7cc77f69b72046fc9a6c42f45e77c7
khairullovruslan <khairullov.ruslan@gmail.com>
2024-10-19 15:03:18 +0300

    feat: created a function to save trip

--- src/main/java/org/tomato/gowithtomato/service/TripService.java
+++ src/main/java/org/tomato/gowithtomato/service/TripService.java
@@ -2,6 +2,7 @@ package org.tomato.gowithtomato.service;
 
 import org.tomato.gowithtomato.dao.TripDAOImpl;
 import org.tomato.gowithtomato.dto.TripDTO;
+import org.tomato.gowithtomato.dto.UserDTO;
 import org.tomato.gowithtomato.entity.Trip;
 import org.tomato.gowithtomato.mapper.TripMapper;
 
@@ -13,15 +14,20 @@ public class TripService {
     private final static TripService INSTANCE = new TripService();
     private final TripDAOImpl tripDAO;
     private final TripMapper tripMapper;
+    private final RouteService routeService;
     private TripService(){
         tripDAO = TripDAOImpl.getInstance();
         tripMapper = TripMapper.getInstance();
+        routeService = RouteService.getInstance();
     }
 
     public static TripService getInstance() {
         return INSTANCE;
     }
-    public void saveTrip(TripDTO tripDTO, Long id){
+
+    public void saveTrip(UserDTO userDTO, TripDTO tripDTO, Long id){
+        tripDTO.setOwner(userDTO);
+        tripDTO.setRoute(routeService.findById(id));
         tripDAO.save(tripMapper.convertDTOToTrip(tripDTO), id);
     }
     public List<TripDTO> findAll(){
