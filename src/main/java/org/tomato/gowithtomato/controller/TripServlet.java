bc1367ed84b012a977923ec5aa8fa066d4ec5a81
khairullovruslan <khairullov.ruslan@gmail.com>
2024-10-19 15:03:51 +0300

    refactor: separated the logic into different methods and classes

--- src/main/java/org/tomato/gowithtomato/controller/TripServlet.java
+++ src/main/java/org/tomato/gowithtomato/controller/TripServlet.java
@@ -4,60 +4,85 @@ package org.tomato.gowithtomato.controller;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.thymeleaf.context.WebContext;
 import org.tomato.gowithtomato.dto.TripDTO;
+import org.tomato.gowithtomato.dto.UserDTO;
+import org.tomato.gowithtomato.exception.IncorrectRequestParametersException;
+import org.tomato.gowithtomato.service.SessionAndCookieService;
 import org.tomato.gowithtomato.service.TripParticipantsService;
 import org.tomato.gowithtomato.service.TripService;
+import org.tomato.gowithtomato.util.DateFormatter;
 
 import java.io.IOException;
-import java.time.format.DateTimeFormatter;
 
+@Slf4j
 @WebServlet("/trip/*")
 public class TripServlet extends BaseServlet {
     private TripService tripService;
     private TripParticipantsService tripParticipantsService;
+    private SessionAndCookieService sessionAndCookieService;
+    private DateFormatter dateFormatter;
+
     @Override
     public void init() {
         super.init();
-        tripService = TripService.getInstance();
-        tripParticipantsService = TripParticipantsService.getInstance();
+        sessionAndCookieService = (SessionAndCookieService) this.getServletContext().getAttribute("sessionAndCookieService");
+        tripService = (TripService) this.getServletContext().getAttribute("tripService");
+        tripParticipantsService = (TripParticipantsService) this.getServletContext().getAttribute("tripParticipantsService");
+        dateFormatter = (DateFormatter) this.getServletContext().getAttribute("dateFormatter");
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String pathInfo = req.getPathInfo();
+        if (pathInfo != null && !pathInfo.isEmpty()) {
+            try {
+                String tripIdString = pathInfo.substring(1);
+                long tripId = Long.parseLong(tripIdString);
+                WebContext context = thymeleafUtil.buildWebContext(req, resp, getServletContext());
+                TripDTO tripDTO = setFormattedDateTime(tripService.findById(tripId));
+
+                context.setVariable("tripInfo", generateTripInfo(tripDTO));
+                context.setVariable("trip", tripDTO);
+                context.setVariable("members", tripParticipantsService.findUsersByTripId(tripId));
+
+                processTemplate(context, "trip", req, resp);
+            } catch (NumberFormatException e) {
+                log.error("Неверный формат идентификатора поездки: {}", pathInfo, e);
+                throw new IncorrectRequestParametersException();
+            }
+        } else {
+            throw new IncorrectRequestParametersException();
+        }
+    }
 
 
-
-
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
         String pathInfo = req.getPathInfo();
 
         if (pathInfo != null && !pathInfo.isEmpty()) {
-            String tripIdString = pathInfo.substring(1);
-            try {
-                WebContext context = thymeleafUtil.buildWebContext(req, resp, getServletContext());
-                long tripId = Long.parseLong(tripIdString);
-                TripDTO tripDTO = tripService.findById(tripId);
-                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE, d MMMM");
-                String formattedDate = tripDTO.getTripDateTime().format(formatter);
-
-                tripDTO.setTripDateTimeFormatted(formattedDate);
-                DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm");
-                String time = tripDTO.getTripDateTime().format(timeFormatter);
-
-                String fromCity = tripDTO.getRoute().getStart().getName();
-                String toCity = tripDTO.getRoute().getFinish().getName();
-
-                String tripInfo = time + " | " + fromCity + " → " + toCity;
-                context.setVariable("tripInfo", tripInfo);
-                context.setVariable("trip", tripDTO);
-                context.setVariable("members", tripParticipantsService.findUsersByTripId(tripId));
-                processTemplate(context, "trip", req, resp);
-            } catch (NumberFormatException e) {
-                resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid trip ID.");
-            }
-        } else {
-            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Trip ID is required.");
+            String routeIdString = pathInfo.substring(1);
+            long tripId = Long.parseLong(routeIdString);
+            UserDTO user = sessionAndCookieService.findUser(req);
+            tripParticipantsService.save(tripId, user.getId());
         }
+        else {
+            throw new IncorrectRequestParametersException();
+        }
+    }
+
+    private TripDTO setFormattedDateTime(TripDTO tripDTO) {
+        String formattedDate = dateFormatter.formatTripDateTime(tripDTO.getTripDateTime());
+        tripDTO.setTripDateTimeFormatted(formattedDate);
+        return tripDTO;
+    }
+
+    private String generateTripInfo(TripDTO tripDTO) {
+        String fromCity = tripDTO.getRoute().getStart().getName();
+        String toCity = tripDTO.getRoute().getFinish().getName();
+        return dateFormatter.formatTripInfo(tripDTO.getTripDateTime(), fromCity, toCity);
     }
 
 }
