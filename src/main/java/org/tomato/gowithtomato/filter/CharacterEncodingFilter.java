7a88c1e0586e9d03ed59402e14fce5519499ab7d
khairullovruslan <khairullov.ruslan@gmail.com>
2024-10-19 14:53:42 +0300

    feat: added a filter for correct encoding

---
+++ src/main/java/org/tomato/gowithtomato/filter/CharacterEncodingFilter.java
@@ -0,0 +1,20 @@
+package org.tomato.gowithtomato.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebFilter("/*")
+public class CharacterEncodingFilter extends HttpFilter {
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        req.setCharacterEncoding("utf-8");
+        res.setCharacterEncoding("utf-8");
+        chain.doFilter(req, res);
+    }
+}
