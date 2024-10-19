d572a43c59357647bd23b4afa045efd3e1bb402e
khairullovruslan <khairullov.ruslan@gmail.com>
2024-10-19 14:55:42 +0300

    feat: added servlet for page with errors

---
+++ src/main/java/org/tomato/gowithtomato/controller/ExceptionServlet.java
@@ -0,0 +1,19 @@
+package org.tomato.gowithtomato.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.thymeleaf.context.WebContext;
+
+import java.io.IOException;
+
+@WebServlet("/error")
+public class ExceptionServlet extends BaseServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        WebContext context = thymeleafUtil.buildWebContext(req, resp, getServletContext());
+        context.setVariable("errorMessage", req.getParameter("errorMessage"));
+        processTemplate(context,"error", req, resp);
+    }
+}
