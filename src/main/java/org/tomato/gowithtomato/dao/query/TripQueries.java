dcbd3d0cfbcf5e977c92d9f9a643223a616cdb21
khairullovruslan <khairullov.ruslan@gmail.com>
2024-10-21 13:36:41 +0300

    feat: added class with sql queries

---
+++ src/main/java/org/tomato/gowithtomato/dao/query/TripQueries.java
@@ -0,0 +1,32 @@
+package org.tomato.gowithtomato.dao.query;
+
+public class TripQueries {
+
+    public static final String SAVE_SQL = """
+        INSERT INTO trip(user_id, route_id, trip_date_time, available_seats, price, status)
+        VALUES (?, ?, ?, ?, ?, ?)
+    """;
+
+    public static final String FIND_ALL_SQL = """
+        SELECT * FROM trip
+    """;
+
+    public static final String ADD_NEW_MEMBER_SQL = """
+        UPDATE trip
+        SET available_seats = available_seats - 1
+        WHERE id = ? AND available_seats > 0
+    """;
+
+    public static final String FIND_BY_ID_SQL = """
+        SELECT * FROM trip
+        WHERE id = ?
+    """;
+
+    public static final String SQL_FILTER = """
+        SELECT t.*
+        FROM trip t
+        JOIN route r ON t.route_id = r.id
+        JOIN Point p1 ON r.start_point_id = p1.id
+        JOIN Point p2 ON r.finish_point_id = p2.id
+    """;
+}
