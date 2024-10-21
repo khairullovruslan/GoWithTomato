58b294707d90749925c01f861b47ad8ae34d32a9
khairullovruslan <khairullov.ruslan@gmail.com>
2024-10-21 13:46:42 +0300

    refactor: improved readability

--- src/main/java/org/tomato/gowithtomato/listener/GoWithTomatoBaseListener.java
+++ src/main/java/org/tomato/gowithtomato/listener/GoWithTomatoBaseListener.java
@@ -5,7 +5,6 @@ import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
 import jakarta.servlet.annotation.WebListener;
-import org.modelmapper.ModelMapper;
 import org.thymeleaf.TemplateEngine;
 import org.tomato.gowithtomato.service.*;
 import org.tomato.gowithtomato.util.*;
@@ -15,24 +14,30 @@ public class GoWithTomatoBaseListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         ServletContext servletContext = sce.getServletContext();
+        TemplateEngine templateEngine = ThymeleafUtil.getInstance().buildTemplateEngine(servletContext);
 
 
         servletContext.setAttribute("authService", new AuthService());
         servletContext.setAttribute("userService", UserService.getInstance());
-        TemplateEngine templateEngine = ThymeleafUtil.getInstance().buildTemplateEngine(servletContext);
-        servletContext.setAttribute("templateEngine", templateEngine);
-        servletContext.setAttribute("thymeleafUtil", ThymeleafUtil.getInstance());
-        servletContext.setAttribute("exceptionHandler", new ExceptionHandler(templateEngine, servletContext));;
+        servletContext.setAttribute("tripService", TripService.getInstance());
+        servletContext.setAttribute("routeService", RouteService.getInstance());
         servletContext.setAttribute("graphHopperApiService", GraphHopperApiService.getInstance());
-        servletContext.setAttribute("objectMapper", new ObjectMapper());
+        servletContext.setAttribute("sessionAndCookieService", SessionAndCookieService.getInstance());
+        servletContext.setAttribute("tripParticipantsService", TripParticipantsService.getInstance());
+
 
         servletContext.setAttribute("ajaxUtil", AjaxUtil.getInstance());
-        servletContext.setAttribute("routeService", RouteService.getInstance());
-        servletContext.setAttribute("sessionAndCookieService", SessionAndCookieService.getInstance());
-        servletContext.setAttribute("tripService", TripService.getInstance());
-        servletContext.setAttribute("tripParticipantsService", TripParticipantsService.getInstance());
+        servletContext.setAttribute("thymeleafUtil", ThymeleafUtil.getInstance());
         servletContext.setAttribute("filterGenerator", new FilterGenerator());
         servletContext.setAttribute("dateFormatter", new DateFormatter());
+        servletContext.setAttribute("templateEngine", templateEngine);
+        servletContext.setAttribute("exceptionHandler", new ExceptionHandler(templateEngine, servletContext));
+        servletContext.setAttribute("objectMapper", new ObjectMapper());
+
+
+
+
+
 
 
 
