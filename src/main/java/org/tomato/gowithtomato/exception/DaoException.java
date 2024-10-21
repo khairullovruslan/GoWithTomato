3072e1503e0e0c50a27ca0084b7090fe199012a6
khairullovruslan <khairullov.ruslan@gmail.com>
2024-10-21 13:33:09 +0300

    refactor: added new constructors

--- src/main/java/org/tomato/gowithtomato/exception/DaoException.java
+++ src/main/java/org/tomato/gowithtomato/exception/DaoException.java
@@ -1,4 +1,13 @@
 package org.tomato.gowithtomato.exception;
 
 public class DaoException extends RuntimeException{
+    public DaoException() {
+        super("Ошибка DAO");
+    }
+    public DaoException(String message, Throwable e ){
+        super(message, e);
+    }
+    public DaoException(String message) {
+        super(message);
+    }
 }
