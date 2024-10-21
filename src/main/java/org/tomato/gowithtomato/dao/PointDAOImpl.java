e983d5fdf1172e5b105fe0517a7d90b48aae1202
khairullovruslan <khairullov.ruslan@gmail.com>
2024-10-21 13:35:53 +0300

    refactor: improved readability

--- src/main/java/org/tomato/gowithtomato/dao/PointDAOImpl.java
+++ src/main/java/org/tomato/gowithtomato/dao/PointDAOImpl.java
@@ -3,43 +3,27 @@ package org.tomato.gowithtomato.dao;
 import org.tomato.gowithtomato.dao.daoInterface.PointDao;
 import org.tomato.gowithtomato.entity.Point;
 import org.tomato.gowithtomato.exception.DaoException;
+import org.tomato.gowithtomato.exception.UniqueSqlException;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
+import static org.tomato.gowithtomato.dao.query.PointQueries.*;
 
 public class PointDAOImpl implements PointDao {
     private static final PointDAOImpl INSTANCE = new PointDAOImpl();
 
     private PointDAOImpl() {}
 
-    private static final String SAVE_SQL =
-        """
-        INSERT INTO point(lat, lng, name, country, state, osm_value) VALUES (?, ?, ?, ?, ?, ?)  
-        ON CONFLICT (lat, lng) DO UPDATE SET name = excluded.name, country = excluded.country, state = excluded.state, osm_value = excluded.osm_value  
-        RETURNING id
-        """;
-
-    private static final String FIND_BY_COORDINATE_SQL =
-        """
-        SELECT * from point WHERE lat = ? AND lng = ?
-        """;
-
-    private static final String FIND_BY_ID_SQL =
-            """
-            SELECT * from point WHERE id = ?
-            """;
-
     public static PointDAOImpl getInstance() {
         return INSTANCE;
     }
 
+    @Override
     public Point save(Connection connection, Point entity) throws DaoException, SQLException {
-        try (PreparedStatement statement = connection.prepareStatement(SAVE_SQL)) {
+        try (PreparedStatement statement = connection.prepareStatement(SAVE_SQL, Statement.RETURN_GENERATED_KEYS)) {
             statement.setDouble(1, entity.getLat());
             statement.setDouble(2, entity.getLng());
             statement.setString(3, entity.getName());
@@ -47,46 +31,70 @@ public class PointDAOImpl implements PointDao {
             statement.setString(5, entity.getState());
             statement.setString(6, entity.getOsmValue());
 
-            try (ResultSet keys = statement.executeQuery()) {
+            int affectedRows = statement.executeUpdate();
+            if (affectedRows == 0) {
+                throw new DaoException("Не удалось сохранить точку.");
+            }
+
+            try (ResultSet keys = statement.getGeneratedKeys()) {
                 if (keys.next()) {
                     entity.setId(keys.getLong(1));
                 }
             }
             return entity;
+        } catch (SQLException e) {
+            handleSQLException(e);
+            throw new DaoException("Ошибка при сохранении точки.", e);
         }
     }
 
+    @Override
     public Optional<Point> findByLatLng(Connection connection, Double lat, Double lng) throws SQLException {
         try (PreparedStatement statement = connection.prepareStatement(FIND_BY_COORDINATE_SQL)) {
             statement.setDouble(1, lat);
             statement.setDouble(2, lng);
             ResultSet result = statement.executeQuery();
-            List<Point> points = convertResultSetToList(result);
-            return points.isEmpty() ? Optional.empty() : Optional.of(points.get(0));
+            return convertResultSetToOptional(result);
         }
     }
 
+    @Override
     public Optional<Point> findById(Connection connection, Long id) throws SQLException {
         try (PreparedStatement statement = connection.prepareStatement(FIND_BY_ID_SQL)) {
             statement.setLong(1, id);
             ResultSet result = statement.executeQuery();
-            List<Point> points = convertResultSetToList(result);
-            return points.isEmpty() ? Optional.empty() : Optional.of(points.get(0));
+            return convertResultSetToOptional(result);
         }
     }
+
+    private Optional<Point> convertResultSetToOptional(ResultSet result) throws SQLException {
+        List<Point> points = convertResultSetToList(result);
+        return points.isEmpty() ? Optional.empty() : Optional.of(points.getFirst());
+    }
+
     private List<Point> convertResultSetToList(ResultSet result) throws SQLException {
         List<Point> points = new ArrayList<>();
         while (result.next()) {
-            points.add(Point.builder()
-                    .id(result.getLong("id"))
-                    .lng(result.getDouble("lng"))
-                    .lat(result.getDouble("lat"))
-                    .name(result.getString("name"))
-                    .state(result.getString("state"))
-                    .osmValue(result.getString("osm_value"))
-                    .country(result.getString("country"))
-                    .build());
+            points.add(createPointFromResultSet(result));
         }
         return points;
+    }
+
+    private Point createPointFromResultSet(ResultSet result) throws SQLException {
+        return Point.builder()
+                .id(result.getLong("id"))
+                .lng(result.getDouble("lng"))
+                .lat(result.getDouble("lat"))
+                .name(result.getString("name"))
+                .state(result.getString("state"))
+                .osmValue(result.getString("osm_value"))
+                .country(result.getString("country"))
+                .build();
+    }
+
+    private void handleSQLException(SQLException e) throws UniqueSqlException {
+        if ("23505".equals(e.getSQLState())) {
+            throw new UniqueSqlException();
+        }
     }
 }
--- src/main/java/org/tomato/gowithtomato/dao/TripDAOImpl.java
+++ src/main/java/org/tomato/gowithtomato/dao/TripDAOImpl.java
@@ -5,7 +5,7 @@ import lombok.SneakyThrows;
 import org.tomato.gowithtomato.dao.daoInterface.TripDAO;
 import org.tomato.gowithtomato.entity.*;
 import org.tomato.gowithtomato.exception.DaoException;
-
+import static org.tomato.gowithtomato.dao.query.TripQueries.*;
 import java.sql.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -20,29 +20,15 @@ public class TripDAOImpl implements TripDAO {
         userDAO = UserDAOImpl.getInstance();
         routeDAO = RouteDAOImpl.getInstance();
     }
-    private final static String  SAVE_SQL =
-            """
-                 INSERT  INTO trip(USER_ID, ROUTE_ID, TRIP_DATE_TIME, AVAILABLE_SEATS, PRICE, STATUS)
-                 VALUES (?, ?, ?, ?, ?, ?)
-            """;
-    private final static String FIND_ALL_SQL =
-            """
-            SELECT * from trip
-            """;
-    private final static String ADD_NEW_MEMBER_SQL = """
-            UPDATE trip
-            SET available_seats = available_seats - 1
-            WHERE id = ? AND available_seats > 0;
-            """;
 
-    private final static String FIND_BY_ID_SQL = " SELECT * from trip where id = ?";
+
 
     public static TripDAOImpl getInstance() {
         return INSTANCE;
     }
 
-    @Override
-    public Trip save(Trip trip, Long id) {
+
+    public Trip saveWithRouteId(Trip trip, Long id) {
         try (var connection = connectionManager.get();
         var statement = connection.prepareStatement(SAVE_SQL, Statement.RETURN_GENERATED_KEYS)){
             statement.setLong(1, trip.getOwner().getId());
@@ -57,32 +43,37 @@ public class TripDAOImpl implements TripDAO {
                 trip.setId(keys.getLong("id"));
                 return trip;
             }
-            throw new DaoException();
+            throw new DaoException("Не удалось получить id для новой поездки.");
 
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new DaoException("Ошибка при сохранении поездки.", e);
         }
     }
 
     @SneakyThrows
     @Override
     public Optional<Trip> findById(Long id) {
-        @Cleanup var connection = connectionManager.get();
-        return findById(connection, id);
+        try (Connection connection = connectionManager.get()) {
+            return findById(connection, id);
+        } catch (SQLException e) {
+            throw new DaoException("Ошибка при поиске поездки по id: " + id, e);
+        }
     }
 
     public Optional<Trip> findById(Connection connection, Long id) throws SQLException {
-       var statement = connection.prepareStatement(FIND_BY_ID_SQL);
+        @Cleanup var statement = connection.prepareStatement(FIND_BY_ID_SQL);
        statement.setLong(1, id);
        List<Trip> trips = convertResultSetToList(connection, statement.executeQuery());
        return trips.size() == 1 ? Optional.ofNullable(trips.getFirst()) : Optional.empty();
     }
 
     @SneakyThrows
-    @Override
     public List<Trip> findAll() {
-        @Cleanup Connection connection = connectionManager.get();
-        return findAll(connection);
+        try (Connection connection = connectionManager.get()) {
+            return findAll(connection);
+        } catch (SQLException e) {
+            throw new DaoException("Ошибка при получении всех поездок", e);
+        }
 
     }
     public List<Trip> findAll(Connection connection) throws SQLException {
@@ -106,74 +97,77 @@ public class TripDAOImpl implements TripDAO {
     public void delete(Long id) {
 
     }
-
-    private List<Trip> convertResultSetToList(Connection connection, ResultSet result) throws SQLException {
-        List<Trip> points = new ArrayList<>();
-        while (result.next()) {
-            Optional<User> owner = userDAO.findById(connection, result.getLong("user_id"));
-            Optional<Route> route = routeDAO.findById(connection, result.getLong("route_id"));
-            if (owner.isEmpty() || route.isEmpty()){
-                throw  new DaoException();
-            }
-            points.add(Trip.builder()
-                    .id(result.getLong("id"))
-                    .price(result.getBigDecimal("price"))
-                    .availableSeats(result.getInt("available_seats"))
-                    .tripDateTime(result.getTimestamp("trip_date_time").toLocalDateTime())
-                    .status(TripStatus.valueOf(result.getString("status")))
-                    .owner(owner.get())
-                    .route(route.get())
-                    .build());
-
-        }
-        return points;
-    }
-
     @SneakyThrows
     public List<Trip> findAllByFilter(Map<String, String> filter) {
-        @Cleanup var connection = connectionManager.get();
-        return findAllByFilter(connection, filter);
+        try (Connection connection = connectionManager.get()) {
+            return findAllByFilter(connection, filter);
+        } catch (SQLException e) {
+            throw new DaoException("Ошибка при поиске с фильтром", e);
+        }
     }
 
 
     public List<Trip> findAllByFilter(Connection connection, Map<String, String> filter) throws SQLException {
-       String query = getQueryByFilter(filter);
-       var statement = connection.prepareStatement(query);
-       return convertResultSetToList(connection, statement.executeQuery());
+        String query = getQueryByFilter(filter);
+        @Cleanup var statement = connection.prepareStatement(query);
+        return convertResultSetToList(connection, statement.executeQuery());
     }
-
-    private String getQueryByFilter(Map<String, String> filter){
-        StringBuilder query = new StringBuilder(
-                """
-                SELECT t.*
-                FROM trip t
-                         JOIN route r ON t.route_id = r.id
-                         JOIN Point p1 ON r.start_point_id = p1.id
-                         JOIN Point p2 ON r.finish_point_id = p2.id
-                """);
-        if (!filter.isEmpty()) query.append(" WHERE ");
-        String[] s = new String[filter.keySet().size()];
-        int count = 0;
-        for (String key : filter.keySet()){
-            switch (key) {
-                case "from" -> s[count++] = String.format("p1.name = '%s'", filter.get(key));
-                case "to" -> s[count++] = String.format("p2.name = '%s'", filter.get(key));
-                case "count" -> s[count++] = String.format("t.available_seats >= '%s'", filter.get(key));
-                case "date" -> {
-                    s[count++] = String.format("t.trip_date_time >= '%s'", LocalDateTime.parse(filter.get(key)).format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
-                }
-            }
-        }
-        query.append(String.join(" and ", s));
-        return query.toString();
-    }
-
     @SneakyThrows
     public boolean addNewMember(Connection connection, Long tripId) {
         var statement = connection.prepareStatement(ADD_NEW_MEMBER_SQL);
         statement.setLong(1, tripId);
         int updated  = statement.executeUpdate();
         return updated > 0;
-
     }
+
+    private List<Trip> convertResultSetToList(Connection connection, ResultSet result) throws SQLException {
+        List<Trip> points = new ArrayList<>();
+        while (result.next()) {
+            points.add(createTripFromResultSet(connection, result));
+        }
+        return points;
+    }
+    private Trip createTripFromResultSet(Connection connection, ResultSet result) throws SQLException {
+        Optional<User> owner = userDAO.findById(connection, result.getLong("user_id"));
+        Optional<Route> route = routeDAO.findById(connection, result.getLong("route_id"));
+        if (owner.isEmpty() || route.isEmpty()){
+            throw  new DaoException("Ошибка при поиске owner || route для поездки");
+        }
+        return Trip.builder()
+                .id(result.getLong("id"))
+                .price(result.getBigDecimal("price"))
+                .availableSeats(result.getInt("available_seats"))
+                .tripDateTime(result.getTimestamp("trip_date_time").toLocalDateTime())
+                .status(TripStatus.valueOf(result.getString("status")))
+                .owner(owner.get())
+                .route(route.get())
+                .build();
+    }
+
+
+
+    private String getQueryByFilter(Map<String, String> filter){
+        StringBuilder query = new StringBuilder(SQL_FILTER);
+        List<String> conditions = new ArrayList<>();
+        filter.forEach((key, value) -> {
+            switch (key) {
+                case "from" -> conditions.add("p1.name = '" + value + "'");
+                case "to" -> conditions.add("p2.name = '" + value + "'");
+                case "count" -> conditions.add("t.available_seats >= '" + value + "'");
+                case "date" -> {
+                    String formattedDate = LocalDateTime.parse(value)
+                            .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+                    conditions.add("t.trip_date_time >= '" + formattedDate + "'");
+                }
+            }
+        });
+
+        if (!conditions.isEmpty()) {
+            query.append(" WHERE ").append(String.join(" AND ", conditions));
+        }
+
+        return query.toString();
+    }
+
+
 }
