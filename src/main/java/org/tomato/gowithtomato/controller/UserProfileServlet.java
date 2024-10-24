7718a6176ee29f057b4dddd1f496a13822e4e46a
khairullovruslan <khairullov.ruslan@gmail.com>
2024-10-24 20:47:42 +0300

    refactor: remove route list

--- src/main/java/org/tomato/gowithtomato/controller/UserProfileServlet.java
+++ src/main/java/org/tomato/gowithtomato/controller/UserProfileServlet.java
@@ -1,16 +1,15 @@
 package org.tomato.gowithtomato.controller;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-import org.thymeleaf.context.WebContext;
-import org.tomato.gowithtomato.dto.RouteDTO;
 import org.tomato.gowithtomato.dto.UserDTO;
 import org.tomato.gowithtomato.service.RouteService;
 import org.tomato.gowithtomato.service.UserService;
 
-import java.util.List;
+import java.io.IOException;
 
 
 @WebServlet("/profile")
@@ -25,22 +24,21 @@ public class UserProfileServlet extends BaseServlet {
     }
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
         HttpSession session = req.getSession();
         String name = req.getParameter("u");
-        WebContext context = thymeleafUtil.buildWebContext(req, resp, getServletContext());
         if (name == null){
             String login = (String) session.getAttribute("user");
             UserDTO user = userService.findUserByLogin(login);
-            context.setVariable("userData", user);
-            context.setVariable("isOwner", true);
-            List<RouteDTO> routeDTOList = routeService.findByUser(user);
-            context.setVariable("routeList", routeDTOList);
-            processTemplate(context, "profile", req, resp);
+            req.setAttribute("userData", user);
+            req.setAttribute("isOwner", true);
         }
+
         else{
-            context.setVariable("userData",  userService.findUserByLogin(name));
-            processTemplate(context, "profile", req, resp);
+            req.setAttribute("userData",  userService.findUserByLogin(name));
         }
+        req.getRequestDispatcher("templates/profile.jsp").forward(req, resp);
+
     }
 }
