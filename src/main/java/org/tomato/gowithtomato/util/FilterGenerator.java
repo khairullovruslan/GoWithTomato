5fc992d7df76be83a07ee69c0d7f3d8763e9b2df
khairullovruslan <khairullov.ruslan@gmail.com>
2024-10-24 20:07:54 +0300

    feat: added page to filter

--- src/main/java/org/tomato/gowithtomato/util/FilterGenerator.java
+++ src/main/java/org/tomato/gowithtomato/util/FilterGenerator.java
@@ -1,36 +1,41 @@
 package org.tomato.gowithtomato.util;
 
 import jakarta.servlet.http.HttpServletRequest;
-import org.thymeleaf.context.WebContext;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class FilterGenerator {
-    public Map<String, String> generateFilter(WebContext context, HttpServletRequest req) {
+    public Map<String, String> generateFilter(HttpServletRequest req) {
         Map<String, String> filter = new HashMap<>();
-        processParameter(context, req, filter, "from");
-        processParameter(context, req, filter, "to");
-        processParameter(context, req, filter, "count");
+        processParameter(req, filter, "from");
+        processParameter(req, filter, "to");
+        processParameter(req, filter, "count");
+        String page = req.getParameter("currentPage");
+        page = page == null ? "1" : page;
+        filter.put("page", page);
 
         String date = req.getParameter("date");
         if (date != null) {
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
             LocalDateTime localDateTime = LocalDate.parse(date, formatter).atStartOfDay();
-            context.setVariable("date", localDateTime.format(formatter));
+            req.setAttribute("date", localDateTime.format(formatter));
             filter.put("date", localDateTime.toString());
         }
+        System.out.println(page);
+        req.setAttribute("page", Integer.parseInt(page));
 
         return filter;
     }
 
-    private void processParameter(WebContext context, HttpServletRequest req, Map<String, String> filter, String paramName) {
+    private void processParameter(HttpServletRequest req, Map<String, String> filter, String paramName) {
         String paramValue = req.getParameter(paramName);
         if (paramValue != null) {
-            context.setVariable(paramName, paramValue);
+            req.setAttribute(paramName, paramValue);
             filter.put(paramName, capitalizeString(paramValue));
         }
     }
