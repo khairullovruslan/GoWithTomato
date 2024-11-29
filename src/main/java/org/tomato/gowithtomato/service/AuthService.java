8e061b54e4c0b797a0473ee948f2d22414d082b9
khairullovruslan <khairullov.ruslan@gmail.com>
2024-11-29 17:34:32 +0300

    refactor: changed UserDTO to UserRegistrationDto

--- src/main/java/org/tomato/gowithtomato/service/AuthService.java
+++ src/main/java/org/tomato/gowithtomato/service/AuthService.java
@@ -1,61 +1,68 @@
 package org.tomato.gowithtomato.service;
 
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
-import org.modelmapper.ModelMapper;
 import org.tomato.gowithtomato.dao.impl.UserDAOImpl;
 import org.tomato.gowithtomato.dto.UserDTO;
-import org.tomato.gowithtomato.entity.User;
+import org.tomato.gowithtomato.dto.UserRegistrationDto;
 import org.tomato.gowithtomato.exception.auth.WrongPasswordException;
+import org.tomato.gowithtomato.mapper.UserMapper;
 import org.tomato.gowithtomato.util.PasswordUtil;
 
 @Slf4j
 public class AuthService {
     private final UserDAOImpl userDao;
-    private final  ModelMapper mapper;
-    private final PasswordUtil passwordUtil;
-    private final CookieService cookieService;
     private final UserService userService;
 
+    private final UserMapper userMapper;
 
-    public AuthService() {
+    private final static AuthService INSTANCE = new AuthService();
+
+
+    private AuthService() {
         this.userService = UserService.getInstance();
-        this.cookieService = CookieService.getInstance();
-        this.passwordUtil = PasswordUtil.getInstance();
         this.userDao = UserDAOImpl.getInstance();
-        this.mapper = new ModelMapper();
+        userMapper = UserMapper.getInstance();
     }
-    public void registration(UserDTO userDTO) {
-        String password = passwordUtil.hashPassword(userDTO.getPassword());
-        userDTO.setPassword(password);
-        userDao.save(mapper.map(userDTO, User.class));
-        log.info("Пользователь успешно зарегестрирован");
+
+    public static AuthService getInstance() {
+        return INSTANCE;
+    }
+
+    public void registration(UserRegistrationDto userDTO) {
+
+        userDao.save(userMapper.convertUserRegistrationDTOToUser(userDTO));
+        log.info("Пользователь успешно зарегистрирован");
 
     }
 
-    public void logout(HttpServletRequest req,  HttpServletResponse resp) {
+    public void logout(HttpServletRequest req) {
         HttpSession session = req.getSession(false);
-        if (session != null) {
-            session.invalidate();
-        }
-        cookieService.deleteCookie(resp);
+        if (session != null) session.invalidate();
+
         log.info("Пользователь успешно вышел");
     }
 
-    public void login(HttpServletRequest req, HttpServletResponse res) {
+    public void login(HttpServletRequest req) {
         String login = req.getParameter("login");
-        UserDTO user = userService.findUserByLogin(login);
+        String password = userService.getPasswordByLogin(login);
         String pwd = req.getParameter("pwd");
-        if (passwordUtil.checkPassword(pwd, user.getPassword())) {
-            cookieService.setCookie(res, login);
+
+        if (PasswordUtil.checkPassword(pwd, password)) {
+            HttpSession httpSession = req.getSession();
+            httpSession.setAttribute("user", userService.findUserByLogin(login));
             log.info("Пользователь успешно вошел в систему");
-        }
-        else {
+        } else {
             throw new WrongPasswordException();
         }
+    }
 
+    public boolean authorizationCheck(HttpServletRequest req) {
+        return req.getSession().getAttribute("user") != null;
+    }
 
+    public UserDTO getUser(HttpServletRequest req) {
+        return (UserDTO) req.getSession().getAttribute("user");
     }
 }
