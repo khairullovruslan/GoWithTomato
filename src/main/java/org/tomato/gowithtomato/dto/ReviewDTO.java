3388f03d648969ad64fa9ece2f0ffa91f501fcd0
khairullovruslan <khairullov.ruslan@gmail.com>
2024-11-29 17:44:31 +0300

    refactor: optimize imports

--- src/main/java/org/tomato/gowithtomato/dto/ReviewDTO.java
+++ src/main/java/org/tomato/gowithtomato/dto/ReviewDTO.java
@@ -3,8 +3,6 @@ package org.tomato.gowithtomato.dto;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
-import org.tomato.gowithtomato.entity.Trip;
-import org.tomato.gowithtomato.entity.User;
 
 @Getter
 @Setter
