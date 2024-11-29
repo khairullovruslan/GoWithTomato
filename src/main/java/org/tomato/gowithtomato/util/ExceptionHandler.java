68c2a2f395c4e78088e95096bcaa3818de1c9afa
khairullovruslan <khairullov.ruslan@gmail.com>
2024-11-29 17:35:43 +0300

    feat: remove thymeleaf

--- src/main/java/org/tomato/gowithtomato/util/ExceptionHandler.java
+++ src/main/java/org/tomato/gowithtomato/util/ExceptionHandler.java
@@ -1,62 +1,44 @@
 package org.tomato.gowithtomato.util;
 
 
-import jakarta.servlet.ServletContext;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.ConstraintViolation;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.WebContext;
 import org.tomato.gowithtomato.exception.auth.RegistrationException;
+import org.tomato.gowithtomato.exception.auth.UnauthorizedException;
 import org.tomato.gowithtomato.exception.auth.WrongPasswordException;
 import org.tomato.gowithtomato.exception.common.IncorrectRequestParametersException;
 import org.tomato.gowithtomato.exception.common.ServletInitializationException;
 import org.tomato.gowithtomato.exception.db.RoutNotFoundException;
 import org.tomato.gowithtomato.exception.db.UserNotFoundException;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
 public class ExceptionHandler {
-    private final TemplateEngine templateEngine;
-    private final ServletContext context;
 
-    public ExceptionHandler(TemplateEngine templateEngine, ServletContext servletContext) {
-        this.templateEngine = templateEngine;
-        this.context = servletContext;
-    }
 
-    @SneakyThrows
-    protected void processTemplate(String templateName, HttpServletRequest request, HttpServletResponse response,
-                                   List<String> errorList) {
-        ThymeleafUtil thymeleafUtil = ThymeleafUtil.getInstance();
-        WebContext context = thymeleafUtil.buildWebContext(request, response, this.context);
-        log.info("Processing template: {}", templateName);
-        if (!errorList.isEmpty()) {
-            context.setVariable("errorList", errorList);
-        }
-        templateEngine.process(templateName, context, response.getWriter());
-    }
-
-    public void handle(Exception e, HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    public void handle(Exception e, HttpServletRequest req, HttpServletResponse resp) {
         try {
             switch (e) {
                 case RegistrationException exc -> {
-                    List<String> errors = exc.getViolations().stream()
-                            .map(ConstraintViolation::getMessage)
-                            .toList();
-                    processTemplate("registration", req, resp, errors);
+                    req.setAttribute("errorList",
+                            exc.getViolations()
+                                    .stream()
+                                    .map(ConstraintViolation::getMessage)
+                                    .toList());
+                    req.getRequestDispatcher("templates/registration.jsp").forward(req, resp);
                 }
                 case UserNotFoundException ignored -> {
-                    processTemplate("login", req, resp, new ArrayList<>(List.of("Пользователь с таким логином не был найден")));
+                    req.setAttribute("errorList", new ArrayList<>(List.of("Пользователь с таким логином не был найден")));
+                    req.getRequestDispatcher("templates/login.jsp").forward(req, resp);
                 }
                 case WrongPasswordException ignored -> {
-                    processTemplate("login", req, resp, new ArrayList<>(List.of("Неверный пароль")));
+                    req.setAttribute("errorList", new ArrayList<>(List.of("Неверный пароль")));
+                    req.getRequestDispatcher("templates/login.jsp").forward(req, resp);
                 }
                 case RoutNotFoundException ignored -> {
                     redirectToErrorPage(req, resp, "Маршрут не найден! Попробуйте еще раз.");
@@ -64,6 +46,8 @@ public class ExceptionHandler {
                 case IncorrectRequestParametersException ignored -> {
                     redirectToErrorPage(req, resp, "Некорретные параметры запроса! Попробуйте еще раз.");
                 }
+                case UnauthorizedException ignored ->
+                        req.getRequestDispatcher("templates/login.jsp").forward(req, resp);
                 default -> throw new Exception();
             }
 
