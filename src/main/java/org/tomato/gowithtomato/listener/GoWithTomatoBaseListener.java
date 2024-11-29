bbddb8bfc7e945ace974c4d35d91401c452a9942
khairullovruslan <khairullov.ruslan@gmail.com>
2024-11-29 17:49:45 +0300

    feat: added review list to the user profile

--- src/main/java/org/tomato/gowithtomato/listener/GoWithTomatoBaseListener.java
+++ src/main/java/org/tomato/gowithtomato/listener/GoWithTomatoBaseListener.java
@@ -5,41 +5,33 @@ import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
 import jakarta.servlet.annotation.WebListener;
-import org.thymeleaf.TemplateEngine;
 import org.tomato.gowithtomato.service.*;
-import org.tomato.gowithtomato.util.*;
+import org.tomato.gowithtomato.util.AjaxUtil;
+import org.tomato.gowithtomato.util.DateFormatter;
+import org.tomato.gowithtomato.util.ExceptionHandler;
+import org.tomato.gowithtomato.util.FilterGenerator;
 
 @WebListener
 public class GoWithTomatoBaseListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         ServletContext servletContext = sce.getServletContext();
-        TemplateEngine templateEngine = ThymeleafUtil.getInstance().buildTemplateEngine(servletContext);
 
 
-        servletContext.setAttribute("authService", new AuthService());
+        servletContext.setAttribute("authService", AuthService.getInstance());
         servletContext.setAttribute("userService", UserService.getInstance());
         servletContext.setAttribute("tripService", TripService.getInstance());
         servletContext.setAttribute("routeService", RouteService.getInstance());
         servletContext.setAttribute("graphHopperApiService", GraphHopperApiService.getInstance());
-        servletContext.setAttribute("cookieService", CookieService.getInstance());
         servletContext.setAttribute("tripParticipantsService", TripParticipantsService.getInstance());
         servletContext.setAttribute("reviewService", ReviewService.getInstance());
 
 
         servletContext.setAttribute("ajaxUtil", AjaxUtil.getInstance());
-        servletContext.setAttribute("thymeleafUtil", ThymeleafUtil.getInstance());
         servletContext.setAttribute("filterGenerator", new FilterGenerator());
         servletContext.setAttribute("dateFormatter", new DateFormatter());
-        servletContext.setAttribute("templateEngine", templateEngine);
-        servletContext.setAttribute("exceptionHandler", new ExceptionHandler(templateEngine, servletContext));
+        servletContext.setAttribute("exceptionHandler", new ExceptionHandler());
         servletContext.setAttribute("objectMapper", new ObjectMapper());
-
-
-
-
-
-
 
 
     }
