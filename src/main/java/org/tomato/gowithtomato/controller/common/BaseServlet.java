5aea8c1c672b31950168b0898ca3c4f5b5f4a671
khairullovruslan <khairullov.ruslan@gmail.com>
2024-11-29 17:35:43 +0300

    feat: remove thymeleaf

--- src/main/java/org/tomato/gowithtomato/controller/common/BaseServlet.java
+++ src/main/java/org/tomato/gowithtomato/controller/common/BaseServlet.java
@@ -7,45 +7,25 @@ import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.WebContext;
 import org.tomato.gowithtomato.exception.common.ServletInitializationException;
 import org.tomato.gowithtomato.util.ExceptionHandler;
-import org.tomato.gowithtomato.util.ThymeleafUtil;
 
 
 @Slf4j
 public class BaseServlet extends HttpServlet {
-    protected TemplateEngine templateEngine;
     private ExceptionHandler exceptionHandler;
-    protected ThymeleafUtil thymeleafUtil;
     protected Validator validator;
 
     @Override
     public void init() {
         try (var validatorFactory = Validation.buildDefaultValidatorFactory()) {
-            thymeleafUtil = (ThymeleafUtil) this.getServletContext().getAttribute("thymeleafUtil");
             validator = validatorFactory.getValidator();
-            templateEngine = (TemplateEngine) this.getServletContext().getAttribute("templateEngine");
             exceptionHandler = (ExceptionHandler) this.getServletContext().getAttribute("exceptionHandler");
         } catch (Exception e) {
             exceptionHandler.handle(new ServletInitializationException());
         }
     }
 
-    @SneakyThrows
-    protected void processTemplate(String templateName, HttpServletRequest request, HttpServletResponse response) {
-        WebContext context = thymeleafUtil.buildWebContext(request, response, getServletContext());
-        context.setVariable("context", request.getContextPath());
-        log.info("Шаблон обработки: {}", templateName);
-        templateEngine.process(templateName, context, response.getWriter());
-    }
-
-    @SneakyThrows
-    protected void processTemplate(WebContext context, String templateName, HttpServletRequest request, HttpServletResponse response) {
-        context.setVariable("context", request.getContextPath());
-        templateEngine.process(templateName, context, response.getWriter());
-    }
 
     @Override
     @SneakyThrows
