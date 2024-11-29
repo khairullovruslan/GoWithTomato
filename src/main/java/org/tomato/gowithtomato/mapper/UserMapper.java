9d01bf8bc8c9a558646aa33d92d198bbfaffbf5e
khairullovruslan <khairullov.ruslan@gmail.com>
2024-11-29 17:41:43 +0300

    refactor: changed authService

--- src/main/java/org/tomato/gowithtomato/mapper/UserMapper.java
+++ src/main/java/org/tomato/gowithtomato/mapper/UserMapper.java
@@ -1,39 +1,64 @@
 package org.tomato.gowithtomato.mapper;
-import org.tomato.gowithtomato.dto.PointDTO;
+
 import org.tomato.gowithtomato.dto.UserDTO;
+import org.tomato.gowithtomato.dto.UserRegistrationDto;
 import org.tomato.gowithtomato.entity.User;
+import org.tomato.gowithtomato.util.PasswordUtil;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-public class UserMapper {
+public class UserMapper implements RowMapper<User> {
     private static final UserMapper INSTANCE = new UserMapper();
-    private UserMapper(){
 
+    private UserMapper() {
     }
 
     public static UserMapper getInstance() {
         return INSTANCE;
     }
-    public User convertDTOToUser(UserDTO userDTO){
+
+    @Override
+    public User mapRow(ResultSet result) throws SQLException {
+        return User
+                .builder()
+                .id(result.getLong("id"))
+                .login(result.getString("login"))
+                .password(result.getString("password"))
+                .email(result.getString("email"))
+                .phoneNumber(result.getString("phone_number"))
+                .build();
+    }
+
+    public User convertDTOToUser(UserDTO userDTO) {
         return User
                 .builder()
                 .id(userDTO.getId())
                 .login(userDTO.getLogin())
-                .password(userDTO.getPassword())
                 .phoneNumber(userDTO.getPhoneNumber())
                 .email(userDTO.getEmail())
                 .build();
 
     }
-    public UserDTO convertUserToDTO(User user){
+
+    public UserDTO convertUserToDTO(User user) {
         return UserDTO
                 .builder()
                 .id(user.getId())
                 .login(user.getLogin())
-                .password(user.getPassword())
                 .phoneNumber(user.getPhoneNumber())
                 .email(user.getEmail())
                 .build();
 
     }
 
+    public User convertUserRegistrationDTOToUser(UserRegistrationDto user) {
+        return User
+                .builder()
+                .login(user.login())
+                .phoneNumber(user.phoneNumber())
+                .email(user.email())
+                .password(PasswordUtil.hashPassword(user.password()))
+                .build();
+    }
 }
