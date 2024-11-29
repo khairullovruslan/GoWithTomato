6aaf69f082d43098dd1feb896b29a3afd2fd4034
khairullovruslan <khairullov.ruslan@gmail.com>
2024-11-29 17:32:53 +0300

    feat: added protected pages

--- src/main/java/org/tomato/gowithtomato/filter/AuthFilter.java
+++ src/main/java/org/tomato/gowithtomato/filter/AuthFilter.java
@@ -7,41 +7,53 @@ import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.tomato.gowithtomato.exception.db.UserNotFoundException;
-import org.tomato.gowithtomato.service.CookieService;
+import lombok.extern.slf4j.Slf4j;
+import org.tomato.gowithtomato.service.AuthService;
 
 import java.io.IOException;
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
+import java.util.regex.Pattern;
 
+@Slf4j
 @WebFilter("/*")
 public class AuthFilter extends HttpFilter {
-    private CookieService cookieService;
-
-    private static final Set<String> pagesForAuthorized = new HashSet<>(
-            List.of("")
-    );
+    private AuthService authService;
 
     @Override
     public void init() throws ServletException {
-        cookieService = CookieService.getInstance();
+        super.init();
+        authService = (AuthService) this.getServletContext().getAttribute("authService");
     }
+
+    private static final Map<String, List<String>> pagesForAuthorizedUser = new HashMap<>(Map.of(
+            "PUT", List.of("/trip/.*"),
+            "DELETE", List.of(),
+            "POST", List.of("/review", "/new-route", "/create-trip", "/trip/.*"),
+            "GET", List.of("/review", "/new-route", "/profile/routes", "/create-trip"),
+            "PATCH", List.of()));
+
 
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
-
-        try {
-            req.setAttribute("user", cookieService.findUser(req));
-            super.doFilter(req, res, chain);
-        } catch (UserNotFoundException e) {
-            String path = req.getRequestURI().substring(req.getContextPath().length());
-            if (pagesForAuthorized.contains(path)) {
-                throw e;
-            } else {
-                req.setAttribute("user", null);
+        boolean isPagesForAuthorizedUser = false;
+        System.out.println(req.getRequestURI().substring(req.getContextPath().length()));
+        for (String s : pagesForAuthorizedUser.get(req.getMethod())) {
+            Pattern pattern = Pattern.compile(s);
+            if (pattern.matcher(req.getRequestURI().substring(req.getContextPath().length())).matches()) {
+                isPagesForAuthorizedUser = true;
             }
-
+        }
+        log.info("isAuthorizedPage? : {}", isPagesForAuthorizedUser);
+        if (isPagesForAuthorizedUser && authService.authorizationCheck(req)) {
+            chain.doFilter(req, res);
+        } else if (isPagesForAuthorizedUser && req.getMethod().equals("GET")) {
+            req.getRequestDispatcher("/templates/login.jsp").forward(req, res);
+        } else if (isPagesForAuthorizedUser) {
+            res.sendRedirect(req.getContextPath() + "/login");
+        } else {
+            chain.doFilter(req, res);
         }
 
     }
