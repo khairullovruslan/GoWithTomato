328baef0023a38d018bb04c6070b958f323b3252
khairullovruslan <khairullov.ruslan@gmail.com>
2024-12-12 00:30:04 +0300

    refactor: changed the concat to formatted

--- src/main/java/org/tomato/gowithtomato/util/DateFormatter.java
+++ src/main/java/org/tomato/gowithtomato/util/DateFormatter.java
@@ -4,11 +4,12 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class DateFormatter {
-    public  String format(LocalDateTime dateTime) {
+    public String format(LocalDateTime dateTime) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
         return dateTime.format(formatter);
     }
-    public  String formatTripDateTime(LocalDateTime dateTime) {
+
+    public String formatTripDateTime(LocalDateTime dateTime) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE, d MMMM");
         return dateTime.format(formatter);
     }
@@ -16,6 +17,6 @@ public class DateFormatter {
     public String formatTripInfo(LocalDateTime dateTime, String fromCity, String toCity) {
         DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm");
         String time = dateTime.format(timeFormatter);
-        return time + " | " + fromCity + " → " + toCity;
+        return "%s | %s →  %s".formatted(time, fromCity, toCity);
     }
 }
