d5d4fb40a6d169e5b34cc8115520ede28187a132
khairullovruslan <khairullov.ruslan@gmail.com>
2024-12-12 00:16:17 +0300

    feat: added sql scripts

---
+++ src/main/resources/db/migration/V2__Insert_Admin.sql
@@ -0,0 +1,6 @@
+--insert admin
+insert into users(login, password, email, phone_number)
+values ('admin',
+        '$2a$12$5xM1QS0dYXYkBj06RbV4h.gObJV/veaXYM.M32zUHf5a49a8g.5qO',
+        'admin@admin.com',
+        +79538985918)
