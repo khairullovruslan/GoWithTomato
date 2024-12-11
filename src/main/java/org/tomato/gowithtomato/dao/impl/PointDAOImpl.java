1de1c1039d657c6642be9d7076287a0d55a52a8c
khairullovruslan <khairullov.ruslan@gmail.com>
2024-12-12 00:27:23 +0300

    refactor: changed logic for working with points

--- src/main/java/org/tomato/gowithtomato/dao/impl/PointDAOImpl.java
+++ src/main/java/org/tomato/gowithtomato/dao/impl/PointDAOImpl.java
@@ -3,12 +3,9 @@ package org.tomato.gowithtomato.dao.impl;
 import org.tomato.gowithtomato.dao.daoInterface.PointDAO;
 import org.tomato.gowithtomato.entity.Point;
 import org.tomato.gowithtomato.exception.db.DaoException;
-import org.tomato.gowithtomato.exception.db.UniqueSqlException;
 import org.tomato.gowithtomato.mapper.PointMapper;
 
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 import static org.tomato.gowithtomato.dao.query.PointQueries.*;
@@ -24,6 +21,7 @@ public class PointDAOImpl extends PointDAO {
         return INSTANCE;
     }
 
+    @Override
     public Point save(Point entity) {
         try (Connection connection = getConnection();
              PreparedStatement statement = connection.prepareStatement(SAVE_SQL, Statement.RETURN_GENERATED_KEYS)) {
@@ -35,22 +33,28 @@ public class PointDAOImpl extends PointDAO {
             statement.setString(6, entity.getOsmValue());
 
             int affectedRows = statement.executeUpdate();
+            System.out.println("affr " + affectedRows);
             if (affectedRows == 0) {
                 throw new DaoException("Не удалось сохранить точку.");
             }
+
+
 
             try (ResultSet keys = statement.getGeneratedKeys()) {
                 if (keys.next()) {
                     entity.setId(keys.getLong(1));
                 }
             }
+            System.out.println("ent "  + entity);
             return entity;
         } catch (SQLException e) {
+            e.printStackTrace();
             handleSQLException(e);
             throw new DaoException("Ошибка при сохранении точки.", e);
         }
     }
 
+    @Override
     public Optional<Point> findByLatLng(Double lat, Double lng) {
         try (Connection connection = getConnection();
              PreparedStatement statement = connection.prepareStatement(FIND_BY_COORDINATE_SQL)) {
@@ -63,6 +67,7 @@ public class PointDAOImpl extends PointDAO {
         }
     }
 
+    @Override
     public Optional<Point> findById(Long id) {
         try (Connection connection = getConnection(); PreparedStatement statement = connection.prepareStatement(FIND_BY_ID_SQL)) {
             statement.setLong(1, id);
@@ -73,22 +78,4 @@ public class PointDAOImpl extends PointDAO {
         }
     }
 
-    private Optional<Point> convertResultSetToOptional(ResultSet result) throws SQLException {
-        List<Point> points = convertResultSetToList(result);
-        return points.isEmpty() ? Optional.empty() : Optional.of(points.getFirst());
-    }
-
-    private List<Point> convertResultSetToList(ResultSet result) throws SQLException {
-        List<Point> points = new ArrayList<>();
-        while (result.next()) {
-            points.add(mapper.mapRow(result));
-        }
-        return points;
-    }
-
-    private void handleSQLException(SQLException e) throws UniqueSqlException {
-        if ("23505".equals(e.getSQLState())) {
-            throw new UniqueSqlException("Ошибка уникальности", e);
-        }
-    }
 }
