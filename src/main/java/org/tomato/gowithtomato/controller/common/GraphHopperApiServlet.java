6c26cf587b527dc6abb677c3d406fca6b1cdf8a1
khairullovruslan <khairullov.ruslan@gmail.com>
2024-12-12 00:20:07 +0300

    feat: changed graph hopper logic

--- src/main/java/org/tomato/gowithtomato/controller/common/GraphHopperApiServlet.java
+++ src/main/java/org/tomato/gowithtomato/controller/common/GraphHopperApiServlet.java
@@ -4,14 +4,20 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.tomato.gowithtomato.dto.route.NewRouteResponse;
 import org.tomato.gowithtomato.dto.route.RouteDTO;
+import org.tomato.gowithtomato.exception.common.GraphHopperApiException;
 import org.tomato.gowithtomato.factory.ServiceFactory;
 import org.tomato.gowithtomato.service.GraphHopperApiService;
 import org.tomato.gowithtomato.util.AjaxUtil;
 
 import java.io.IOException;
 
+/*
+ Сервлет для GraphHopperApi
+ */
+@Slf4j
 @WebServlet("/graph-hopper-api")
 public class GraphHopperApiServlet extends BaseServlet {
     private AjaxUtil ajaxUtil;
@@ -29,12 +35,16 @@ public class GraphHopperApiServlet extends BaseServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         NewRouteResponse routeResponse = objectMapper.readValue(req.getInputStream(), NewRouteResponse.class);
-        RouteDTO routeDTO = routeResponse.getRouteDTO();
+        RouteDTO routeDTO = routeResponse.routeDTO();
 
         try {
+            log.info("Получение информации маршрута");
+
+            resp.setStatus(200);
+
             ajaxUtil.senderRouteInfo(resp, graphHopperApiService.getInfo(routeDTO));
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            throw new GraphHopperApiException("Ошибка при получение информации маршрута", e);
         }
 
 
