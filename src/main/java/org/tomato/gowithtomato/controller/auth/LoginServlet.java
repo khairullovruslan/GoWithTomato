774d93abda633207b794d25906a84726d599d384
khairullovruslan <khairullov.ruslan@gmail.com>
2024-12-12 00:29:06 +0300

    refactor: changed auth servlets

--- src/main/java/org/tomato/gowithtomato/controller/auth/LoginServlet.java
+++ src/main/java/org/tomato/gowithtomato/controller/auth/LoginServlet.java
@@ -1,34 +1,64 @@
 package org.tomato.gowithtomato.controller.auth;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.tomato.gowithtomato.controller.common.BaseServlet;
+import org.tomato.gowithtomato.dto.user.UserLoginDTO;
+import org.tomato.gowithtomato.exception.auth.WrongPasswordException;
+import org.tomato.gowithtomato.exception.db.UserNotFoundException;
 import org.tomato.gowithtomato.factory.ServiceFactory;
 import org.tomato.gowithtomato.service.AuthService;
+import org.tomato.gowithtomato.util.AjaxUtil;
 
 import java.io.IOException;
 
+/*
+ Аутентификация
+ */
+@Slf4j
 @WebServlet("/login")
 public class LoginServlet extends BaseServlet {
     private AuthService authService;
+
+    private AjaxUtil ajaxUtil;
+
+    private ObjectMapper objectMapper;
+
 
     @Override
     public void init() {
         super.init();
         authService = ServiceFactory.getAuthService();
+        ajaxUtil = (AjaxUtil) this.getServletContext().getAttribute("ajaxUtil");
+        objectMapper = (ObjectMapper) this.getServletContext().getAttribute("objectMapper");
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("templates/login.jsp").forward(req, resp);
+        req.getRequestDispatcher("/WEB-INF/templates/login.jsp").forward(req, resp);
     }
 
+    /*
+    Аутентификация пользователя
+     */
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        authService.login(req);
-        resp.sendRedirect(req.getContextPath() + "/profile");
+        log.info("Процесс аутентификации....");
+        try {
+            UserLoginDTO userLoginDTO = objectMapper.readValue(req.getInputStream(), UserLoginDTO.class);
+            authService.login(userLoginDTO, req);
+            ajaxUtil.senderRespUrl("%s/profile"
+                    .formatted(req.getContextPath()), resp);
+        } catch (WrongPasswordException ignored) {
+            ajaxUtil.senderErrorMessage("Неправильный пароль", resp);
 
+        } catch (UserNotFoundException ignored) {
+            ajaxUtil.senderErrorMessage("Пользователь не найден", resp);
+
+        }
     }
 }
--- src/main/java/org/tomato/gowithtomato/controller/auth/LogoutServlet.java
+++ src/main/java/org/tomato/gowithtomato/controller/auth/LogoutServlet.java
@@ -3,25 +3,35 @@ package org.tomato.gowithtomato.controller.auth;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.tomato.gowithtomato.controller.common.BaseServlet;
 import org.tomato.gowithtomato.factory.ServiceFactory;
 import org.tomato.gowithtomato.service.AuthService;
 
 import java.io.IOException;
 
-
+/*
+Выход из аккаунта
+*/
+@Slf4j
 @WebServlet("/logout")
 public class LogoutServlet extends BaseServlet {
     private AuthService authService;
+
     @Override
     public void init() {
         super.init();
         authService = ServiceFactory.getAuthService();
     }
 
+
+    /*
+    Выход пользователя
+   */
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        log.info("Процесс выхода из аккаунта");
         authService.logout(req);
-        resp.sendRedirect(req.getContextPath() + "/login");
+        resp.sendRedirect("%s/login".formatted(req.getContextPath()));
     }
 }
