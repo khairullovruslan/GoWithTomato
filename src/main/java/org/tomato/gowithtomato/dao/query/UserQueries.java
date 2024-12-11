fa342af5860bcb9147d71282db367624e0c219be
khairullovruslan <khairullov.ruslan@gmail.com>
2024-12-12 00:17:57 +0300

    refactor: changed userDao func

--- src/main/java/org/tomato/gowithtomato/dao/query/UserQueries.java
+++ src/main/java/org/tomato/gowithtomato/dao/query/UserQueries.java
@@ -10,14 +10,14 @@ public class UserQueries {
 
     //language=sql
     public static final String FIND_BY_LOGIN_SQL = """
-            SELECT id, login, email, phone_number, password
+            SELECT id, login, email, phone_number, password, photo_url
             FROM users
             WHERE login = ?
             """;
 
     //language=sql
     public static final String FIND_BY_ID_SQL = """
-            SELECT id, login, email, phone_number, password
+            SELECT id, login, email, phone_number, password, photo_url
             FROM users
             WHERE id = ?
             """;
@@ -33,7 +33,7 @@ public class UserQueries {
 
     //language=sql
     public static final String FIND_BY_EMAIL_SQL = """
-            SELECT id, login, email, phone_number, password
+            SELECT id, login, email, phone_number, password, photo_url
             FROM users
             WHERE email = ?
             """;
@@ -44,4 +44,41 @@ public class UserQueries {
             SET login = ?, phone_number = ?, email = ?
             where id = ?
             """;
+
+    //language=sql
+    public static final String UPDATE_PHOTO_SQL = """
+            UPDATE users
+            SET photo_url = ?
+            where id = ?
+            """;
+
+    //language=sql
+    public static final String UPDATE_USER_PASSWORD_SQL = """
+            UPDATE users
+            SET password = ?
+            where id = ?
+            """;
+
+    //language=sql
+    public static final String DELETE_USER_BY_ID_SQL = """
+            DELETE FROM users
+            WHERE id = ?
+            """;
+
+    //language=sql
+    public static final String FIND_BY_TRIP_ID_SQL = """
+            SELECT  users.id as id, login, email, phone_number, password, photo_url
+            FROM users
+            JOIN trip t on t.user_id = users.id
+            WHERE t.id = ?
+            """;
+    //language=sql
+    public static final String FIND_BY_ROUTE_ID_SQL = """
+           
+            SELECT  users.id as id, login, email, phone_number, password, photo_url
+            FROM users
+            JOIN route r on r.user_id = users.id
+            WHERE r.id = ?
+            """;
+
 }
