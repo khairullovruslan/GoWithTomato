d2b80d2e83bc52f74d7d1048d4a26eab5655c322
khairullovruslan <khairullov.ruslan@gmail.com>
2024-12-12 00:29:16 +0300

    refactor: changed base servlets

--- src/main/java/org/tomato/gowithtomato/controller/common/ErrorServlet.java
+++ src/main/java/org/tomato/gowithtomato/controller/common/ErrorServlet.java
@@ -4,13 +4,20 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 
+/*
+ Страница ошибки
+ */
+@Slf4j
 @WebServlet("/error")
 public class ErrorServlet extends BaseServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("templates/error.jsp").forward(req, resp);
+        log.info("Произошла ошибка");
+        log.info("Открываем страницу для ошибок");
+        req.getRequestDispatcher("/WEB-INF/templates/error.jsp").forward(req, resp);
     }
 }
