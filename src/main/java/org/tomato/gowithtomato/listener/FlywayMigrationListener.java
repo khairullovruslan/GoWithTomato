25ff2115e5df725857b0dd6cdb6a8e581afe2aa0
khairullovruslan <khairullov.ruslan@gmail.com>
2024-12-12 12:34:16 +0300

    refactor: moved settings to environment

--- src/main/java/org/tomato/gowithtomato/listener/FlywayMigrationListener.java
+++ src/main/java/org/tomato/gowithtomato/listener/FlywayMigrationListener.java
@@ -5,7 +5,6 @@ import jakarta.servlet.ServletContextListener;
 import jakarta.servlet.annotation.WebListener;
 import lombok.extern.slf4j.Slf4j;
 import org.flywaydb.core.Flyway;
-import org.tomato.gowithtomato.util.PropertiesUtil;
 
 @Slf4j
 @WebListener
@@ -13,14 +12,13 @@ public class FlywayMigrationListener implements ServletContextListener {
     private static final String URL_KEY = "db.url";
     private static final String USERNAME_KEY = "db.username";
     private static final String PASSWORD_KEY = "db.password";
-    private final PropertiesUtil propertiesUtil = PropertiesUtil.getInstance();
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         try {
-            String url = propertiesUtil.get(URL_KEY);
-            String user = propertiesUtil.get(USERNAME_KEY);
-            String password = propertiesUtil.get(PASSWORD_KEY);
+            String url = System.getenv(URL_KEY);
+            String user = System.getenv(USERNAME_KEY);
+            String password = System.getenv(PASSWORD_KEY);
 
             Flyway flyway = Flyway
                     .configure()
